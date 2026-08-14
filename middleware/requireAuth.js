ad457f41213e7aca1990284e0f66e06d1547a7ed
khemeracr-hash <khemeracr@gmail.com>
2026-08-14 13:35:08 +0700

    Update requireAuth.js

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -1,1 +1,18 @@
+const jwt = require('jsonwebtoken');
 
+module.exports = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Access Denied: No Token Provided' });
+  }
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET || 'your_secret_key');
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(403).json({ message: 'Invalid Token' });
+  }
+};
