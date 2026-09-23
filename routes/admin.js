d448d20b2faf9b238edb28a5051266a7bb01541b
khemeracr-hash <khemeracr@gmail.com>
2026-09-23 11:57:23 +0700

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -5,11 +5,14 @@ const router = express.Router();
 const {
   getAttendanceSummary,
   getTodayAttendance,
+  exportAttendanceReport
 } = require("../controllers/adminController");
 
 // Admin Dashboard APIs
 
 router.get("/summary", getAttendanceSummary);
+
+router.get("/reports/export", exportAttendanceReport);
 
 router.get("/today", getTodayAttendance);
 
